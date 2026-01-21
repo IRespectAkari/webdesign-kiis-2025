50eed68d0f5e0081fcad88beee0694a2d6235fa2
keigo <x.10.a.s.0820keigo@gmail.com>
2026-01-21 23:51:11 +0900

    maybe finished

--- sc.js
+++ sc.js
@@ -10,20 +10,25 @@ function addData(e) {
 
   if(!name || !mail || !message) return;
 
-  $("main").append(create("p", [`名前：`, name]));
-  $("main").append(create("p", [`選んだ果物：`, fruits]));
-  $("main").append(create("p", [`メッセージ：`, message]));
+  const text = [
+    create("p", [`名前：`, name]),
+    create("p", [`好きな果物：`, fruits]),
+    create("p", [`メッセージ：`, message]),
+  ];
+
+  $("#comment").append(create("div", text, {className: "comment"}));
 
   document.asideForm.reset()
 }
 
-const radio = ["いちご", "バナナ", "もも"]
+const radio = ["いちご", "バナナ", "もも", "りんご", "キウイ"]
   .map(t => [create("input", null, {type: "radio", name: "fruits", value: t}), create("span", t)])
   .map(Wrap("label"));
 const formParts = [
+  create("span", "コメント入力欄"),
   create("span", "お名前"),
   create("input", null, { name: "name", type: "text" }),
-  create("span", "メールアドレス"),
+  create("span", "メールアドレス（任意）"),
   create("input", null, { name: "mail", type: "email" }),
   create("span", "好きな果物を一つ選んでね"),
   create("div", radio, {id: "fruitsRadio"}),
@@ -34,38 +39,51 @@ const formParts = [
 
 const form = create("form", formParts, {name: "asideForm"});
 
-const links = create("div");
+const linklist = {
+  "公式サイト": "https://www.craft-warriors.com/",
+  "リリースページ": "https://translimit.co.jp/posts/cwf",
+  "App Store": "https://apps.apple.com/jp/app/craft-warriors-frontier/id6749217735",
+  "Google Play": "https://play.google.com/store/apps/details?id=jp.co.translimit.dream",
+  "Twitter@craft_warriors": "https://x.com/craft_warriors",
+  "代表Twitter@hiroki_takaba": "https://x.com/hiroki_takaba",
+};
+const aList = Object.entries(linklist).map(([t,url])=>create("a", t, {href: url}));
 
-const mainData = articleData.map(d => {
-  const article = create("article", [
+const links = create("div", aList);
+
+const mainData = articleData.map(d => [
     create("h2", d.title),
-    d.sub,
+    create("span", d.sub),
     d.sections.map(dd => create("section", [
       create("h3", dd.title),
-      dd.p
+      dd.a ? create("a", dd.p, {href: dd.a}) : dd.p
     ]))
-  ].flat());
-  return article;
-});
+  ].flat())
+  .map(Wrap("article"));
 const main = create("main", mainData);
-// const main = create("main", create("article", [
-//   create("h2", "クラフロ配信開始！！"),
-//   "前作のCraft Warriorsがサービス終了して早1年…パワーアップして遂に復活！",
-//   create("section", [
-//     create("h3", "何が違うの？"),
-//     "前作は簡単に言えばクラクラ（クラッシュオブクラン）のボクセルバージョンだったが、今作ではキャラを自ら動かせる！"
-//   ]),
-//   create("section", [
-//     create("h3", "面白い？"),
-//     "おもしろい！自ら組み立てた城から出撃し、攻め入ってくる敵たちを仲間と協力して倒すという展開がとても楽しいよ！",
-//   ])
-// ]));
+
 const header = create("header", "Craft Warriors Frontier 攻略の記し");
 const footer = create("footer", "ふったぁだよ");
-const aside  = create("aside", [create("span", "あさいどだよ"), links, form]);
+const aside  = create("aside", [create("span", "あさいどだよ"), create("span", "公式リンク集"), links, create("hr"), form]);
+
+const commentList = [
+  ["ああああ", "りんご", "楽しい！"],
+  ["キラ", "りんご", "マジお勧め"],
+  ["ゆうと", "バナナ", "4回まで報酬多めだよ！"],
+  ["サトシ", "いちご", "よき"],
+].map(([name, fruits, message]) => {
+  const text = [
+    create("p", [`名前：`, name]),
+    create("p", [`好きな果物：`, fruits]),
+    create("p", [`メッセージ：`, message]),
+  ];
+
+  return create("div", text, {className: "comment"});
+});
+const comments = create("div", [create("h3", "コメント欄")].concat(commentList), {id: "comment"});
 
 // const contents = create("div", [aside, main]);
-const contents = create("div", [aside, main]);
+const contents = create("div", [aside, main, comments]);
 const all = [header, contents, footer];
 $("body").append(...all);
 
